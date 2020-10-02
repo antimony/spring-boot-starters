c843397f3a82e8fe56070f3f769a0cdca1c1e509
Mikhail Surin <antimony@yandex.ru>
2020-10-02 17:38:29 +0300

    Add root build gradle config

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    java
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+repositories {
+    jcenter()
+}
